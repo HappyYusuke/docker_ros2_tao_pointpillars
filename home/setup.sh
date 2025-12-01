1f98198e8cf0b4a70fe83025b902913c942fa635
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2025-12-01 19:26:13 +0900

    HARRP向けに調整したros2_tao_pointpillars (Fork)に変更, それに伴ってファイル置換を削除

--- home/setup.sh
+++ home/setup.sh
@@ -2,16 +2,9 @@
 
 # リポジトリのクローン
 mkdir -p ~/colcon_ws/src && cd ~/colcon_ws/src
-git clone https://github.com/NVIDIA-AI-IOT/ros2_tao_pointpillars.git
+git clone https://github.com/HappyYusuke/ros2_tao_pointpillars.git
 git clone https://github.com/HappyYusuke/harrp.git
 git clone https://github.com/Livox-SDK/livox_ros_driver2.git
-
-# ファイルを置換
-rm ~/colcon_ws/src/ros2_tao_pointpillars/launch/pp_infer_launch.py
-rm ~/colcon_ws/src/ros2_tao_pointpillars/package.xml
-cp ~/colcon_ws/src/harrp/external_files/pp_infer_launch.py ~/colcon_ws/src/ros2_tao_pointpillars/launch/
-cp ~/colcon_ws/src/harrp/external_files/point_cloud2_iterator.hpp ~/colcon_ws/src/ros2_tao_pointpillars/include/pp_infer
-cp ~/colcon_ws/src/harrp/external_files/package.xml ~/colcon_ws/src/ros2_tao_pointpillars/
 
 # 依存関係のパッケージをダウンロード
 cd ~/colcon_ws
